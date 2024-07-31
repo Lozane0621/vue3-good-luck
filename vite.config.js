7b0c685f2d3215b28b6f1a4d6a7a193ae40445eb
任罗振 <luozhen.ren@shinyinfo.com.cn>
2024-07-31 18:58:21 +0800

    fix 404

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
-/* eslint-disable no-undef */
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
