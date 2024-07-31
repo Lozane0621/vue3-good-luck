5a789b030e2955b7b3ad01ec87618029471e56ac
任罗振 <luozhen.ren@shinyinfo.com.cn>
2024-07-31 18:56:30 +0800

    fix 404

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
 /* eslint-disable no-undef */
-const fs = require('fs');
-const path = require('path');
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -27,15 +24,7 @@ export default defineConfig({
           sideEffect: true
         })
       ]
-    }),
-    {
-      name: '404-fallback',
-      writeBundle() {
-        const indexPath = path.resolve(__dirname, 'dist/index.html');
-        const errorPath = path.resolve(__dirname, 'dist/404.html');
-        fs.copyFileSync(indexPath, errorPath);
-      },
-    },
+    })
   ],
   resolve: {
     alias: {
